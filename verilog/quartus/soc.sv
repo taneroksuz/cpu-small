e0d864c14620eb262272616734c5982a9b55814c
Taner Öksüz <taner.oeksuez@outlook.com>
2021-09-24 19:15:10 +0200

    pll removed from design.

--- verilog/quartus/soc.sv
+++ verilog/quartus/soc.sv
@@ -10,10 +10,11 @@ module soc
   timeunit 1ns;
   timeprecision 1ps;
 
-  logic rtc;
-  logic rst_pll;
-  logic clk_pll;
-  logic locked;
+  logic rtc = 0;
+  logic [31 : 0] count = 0;
+
+  logic clk_pll = 0;
+  logic [31 : 0] count_pll = 0;
 
   logic [0  : 0] memory_valid;
   logic [0  : 0] memory_instr;
@@ -48,6 +49,21 @@ module soc
   logic [31 : 0] timer_rdata;
   logic [0  : 0] timer_ready;
   logic [0  : 0] timer_irpt;
+
+  always_ff @(posedge clk) begin
+    if (count == clk_divider_rtc) begin
+      rtc <= ~rtc;
+      count <= 0;
+    end else begin
+      count <= count + 1;
+    end
+    if (count_pll == clk_divider_pll) begin
+      clk_pll <= ~clk_pll;
+      count_pll <= 0;
+    end else begin
+      count_pll <= count_pll + 1;
+    end
+  end
 
   always_comb begin
 
@@ -109,20 +125,9 @@ module soc
 
   end
 
-  assign rst_pll = ~rst;
-
-  pll pll_comp
-  (
-    .refclk (clk),
-    .rst (rst_pll),
-    .outclk_0 (clk_pll),
-    .outclk_1 (rtc),
-    .locked (locked)
-  );
-
   cpu cpu_comp
   (
-    .rst (locked),
+    .rst (rst),
     .clk (clk_pll),
     .memory_valid (memory_valid),
     .memory_instr (memory_instr),
@@ -149,7 +154,7 @@ module soc
 
   uart uart_comp
   (
-    .rst (locked),
+    .rst (rst),
     .clk (clk_pll),
     .uart_valid (uart_valid),
     .uart_instr (uart_instr),
@@ -164,7 +169,7 @@ module soc
 
   timer timer_comp
   (
-    .rst (locked),
+    .rst (rst),
     .clk (clk_pll),
     .rtc (rtc),
     .timer_valid (timer_valid),
