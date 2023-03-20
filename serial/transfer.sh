7566fac04fe0d8f641ae503c7c7f98c84c7c5963
Taner Öksüz <taner.oeksuez@outlook.de>
2023-03-20 16:54:48 +0100

    script edited.

--- serial/transfer.sh
+++ serial/transfer.sh
@@ -3,8 +3,6 @@ set -e
 
 start=`date +%s`
 
-stty -F $SERIAL 115200 cs8 -cstopb -parenb -crtscts
-
 $PYTHON $BASEDIR/serial/write.py $SERIAL $BASEDIR/build/$PROGRAM/elf/$PROGRAM.bin
 
 end=`date +%s`
