360cdcd6d7f216bb6c0962fbf3963540f79bd629
Taner Öksüz <taner.oeksuez@outlook.de>
2023-10-26 10:30:57 +0200

    updated.

---
+++ py/elf2coe.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Transform binary files.')
+    parser.add_argument('--input',required=True,help='file name')
+    parser.add_argument('--address',required=True,help='start address')
+    parser.add_argument('--offset',required=True,help='offset')
+
+    args = parser.parse_args()
+
+    input = open(args.input, "r")
+    address = open(args.address, "r")
+    offset = open(args.offset, "r")
+
+    filename = input
+    bin_file = '{0}.bin'.format(input)
+    coe_file = '{0}.coe'.format(input)
+    start_address = int(address,16)
+    offset = int(offset,16)
+
+    print(coe_file)
+
+    with open(bin_file, 'rb') as f:
+        content = f.read()
+
+    output = open(coe_file, 'wb')
+
+    lines = len(content)
+
+    output.write("memory_initialization_radix=16;\n".encode('ascii'))
+    output.write("memory_initialization_vector=\n".encode('ascii'))
+
+    address = 0
+    while address < offset:
+        if address < start_address:
+            string0 = "00"
+            string1 = "00"
+            string2 = "00"
+            string3 = "00"
+        elif address-start_address < lines:
+            if (address-start_address+3) < lines:
+                string0 = "{:02X}".format(content[address-start_address+3])
+            else:
+                string0 = "00"
+            if (address-start_address+2) < lines:
+                string1 = "{:02X}".format(content[address-start_address+2])
+            else:
+                string1 = "00"
+            if (address-start_address+1) < lines:
+                string2 = "{:02X}".format(content[address-start_address+1])
+            else:
+                string2 = "00"
+            if (address-start_address) < lines:
+                string3 = "{:02X}".format(content[address-start_address])
+            else:
+                string3 = "00"
+        else:
+            string0 = "00"
+            string1 = "00"
+            string2 = "00"
+            string3 = "00"
+        if address<(offset-4):
+            string = string0 + string1 + string2 + string3 + ",\n"
+        else:
+            string = string0 + string1 + string2 + string3 + ";"
+        output.write(string.encode('ascii'))
+        address = address + 4
+
+    output.close()
