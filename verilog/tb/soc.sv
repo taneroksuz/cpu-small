8b2be7fa1d0d1f62ea57031e4e35ec3e83ee00db
Taner Öksüz <taner.oeksuez@outlook.de>
2023-11-08 07:00:31 +0100

    updated.

--- verilog/tb/soc.sv
+++ verilog/tb/soc.sv
@@ -118,6 +118,9 @@ module soc();
   logic [31 : 0] counter = 0;
 
   integer reg_file;
+  integer csr_file;
+  integer pmp_file;
+  integer mem_file;
 
   initial begin
     $readmemh("host.dat", host);
@@ -164,6 +167,57 @@ module soc();
         end
       end
       $fclose(reg_file);
+    end
+  end
+
+  initial begin
+    string filename;
+    if ($value$plusargs("CSRFILE=%s",filename)) begin
+      csr_file = $fopen(filename,"w");
+      for (int i=0; i<stoptime; i=i+1) begin
+        @(posedge clock);
+        if (soc.cpu_comp.csr_comp.csr_in.cwren == 1) begin
+          $fwrite(csr_file,"PERIOD = %t\t",$time);
+          $fwrite(csr_file,"WADDR = %x\t",soc.cpu_comp.csr_comp.csr_in.cwaddr);
+          $fwrite(csr_file,"WDATA = %x\n",soc.cpu_comp.csr_comp.csr_in.cwdata);
+        end
+      end
+      $fclose(csr_file);
+    end
+  end
+
+  initial begin
+    string filename;
+    if ($value$plusargs("PMPFILE=%s",filename)) begin
+      pmp_file = $fopen(filename,"w");
+      for (int i=0; i<stoptime; i=i+1) begin
+        @(posedge clock);
+        if (soc.cpu_comp.pmp_comp.csr_pmp_in.cwren == 1) begin
+          $fwrite(pmp_file,"PERIOD = %t\t",$time);
+          $fwrite(pmp_file,"WADDR = %x\t",soc.cpu_comp.pmp_comp.csr_pmp_in.cwaddr);
+          $fwrite(pmp_file,"WDATA = %x\n",soc.cpu_comp.pmp_comp.csr_pmp_in.cwaddr);
+        end
+      end
+      $fclose(pmp_file);
+    end
+  end
+
+  initial begin
+    string filename;
+    if ($value$plusargs("MEMFILE=%s",filename)) begin
+      mem_file = $fopen(filename,"w");
+      for (int i=0; i<stoptime; i=i+1) begin
+        @(posedge clock);
+        if (soc.bram_comp.bram_valid == 1) begin
+          if (|soc.bram_comp.bram_wstrb == 1) begin
+            $fwrite(mem_file,"PERIOD = %t\t",$time);
+            $fwrite(mem_file,"WADDR = %x\t",soc.bram_comp.bram_addr);
+            $fwrite(mem_file,"WSTRB = %b\t",soc.bram_comp.bram_wstrb);
+            $fwrite(mem_file,"WDATA = %x\n",soc.bram_comp.bram_wdata);
+          end
+        end
+      end
+      $fclose(mem_file);
     end
   end
 
